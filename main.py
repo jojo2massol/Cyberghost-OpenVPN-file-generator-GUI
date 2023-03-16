56f7fcbf91631b98c041deb5c03c0184d36f7dcc
Georges <jojo2massol@gmail.com>
2023-03-16 21:35:17 +0100

    corrected bugs, moved protocol selection

--- main.py
+++ main.py
@@ -16,10 +16,12 @@ url_server_groups = domain + api + "get-server-groups"
 
 
 protocols = {"openvpn": "Open VPN", "openvpn_tcp": "Open VPN TCP",
-             "openvpn_legacy": "Open VPN <= 2.3", "openvpn_tcp_legacy": "Open VPN TCP <= 2.3", "ipsec": "IPsec"}
+             "openvpn_legacy": "Open VPN <= 2.3", "openvpn_tcp_legacy": "Open VPN TCP <= 2.3", "ipsec": "IPsec (useless here)"}
 # may be more locales
 locales = {"en_US": "English", "de_DE": "Deutsch", "fr_FR": "Français", "es_ES": "Español",
            "it_IT": "Italiano", "pt_PT": "Português", "ru_RU": "Русский", "pl_PL": "Polski", "nl_NL": "Nederlands"}
+
+(server_groups, country_code) = ("", "")
 
 try:
     import default_conf
@@ -93,21 +95,6 @@ cookieEntered.grid(column=1, row=0)
 # default_conf.cookie as current value
 cookieEntered.insert(0, cookie)
 
-# create a combobox to choose the protocol
-ttk.Label(labelsFrame, text="Protocol:").grid(column=0, row=1, sticky='W')
-protocol_text = tk.StringVar()
-protocolChosen = ttk.Combobox(
-    labelsFrame, width=18, textvariable=protocol_text, state='readonly')
-protocolChosen['values'] = list(protocols.values())
-protocolChosen.grid(column=1, row=1)
-# default_conf.protocol as current value
-protocolChosen.set(protocols[protocol])
-protocolChosen.bind("<<ComboboxSelected>>", lambda e: diable_coutry_choice())
-
-def diable_coutry_choice():
-    countryChosen.state(['disabled'])
-    server_groupChosen.state(['disabled'])
-    try_status.set("")
 
 # create a combobox to choose the locale
 ttk.Label(labelsFrame, text="Locale:").grid(column=0, row=2, sticky='W')
@@ -198,8 +185,25 @@ ttk.Label(labelsFrame, textvariable=save_status).grid(
 labelsFrame2 = ttk.LabelFrame(win, text='Countries')
 labelsFrame2.grid(column=1, row=0, padx=40, pady=40)
 
+# create a combobox to choose the protocol
+ttk.Label(labelsFrame2, text="Protocol:").grid(column=0, row=0, sticky='W')
+protocol_text = tk.StringVar()
+protocolChosen = ttk.Combobox(
+    labelsFrame2, width=18, textvariable=protocol_text, state='readonly')
+protocolChosen['values'] = list(protocols.values())
+protocolChosen.grid(column=1, row=0)
+# default_conf.protocol as current value
+protocolChosen.set(protocols[protocol])
+protocolChosen.bind("<<ComboboxSelected>>", lambda e: protocolChosen_changed())
+
+def protocolChosen_changed():
+    countryChosen.state(['disabled'])
+    server_groupChosen.state(['disabled'])
+    try_status.set("")
+    try_entries()
+
 # create a combobox to choose the country
-ttk.Label(labelsFrame2, text="Country:").grid(column=0, row=0, sticky='W')
+ttk.Label(labelsFrame2, text="Country:").grid(column=0, row=1, sticky='W')
 country_text = tk.StringVar()
 countryChosen = ttk.Combobox(
     labelsFrame2, width=18, textvariable=country_text, state='readonly')
@@ -208,12 +212,13 @@ countryChosen = ttk.Combobox(
 
 countryChosen['values'] = [country["name"]
                            for country in json.loads(countries)]
-countryChosen.grid(column=1, row=0)
+countryChosen.grid(column=1, row=1)
 # countryChosen.current(0)
 
 
 def on_country_change():
     # get the country code
+    global country_code, server_groups
     country_code = [country["countrycode"] for country in json.loads(
         countries) if country["name"] == countryChosen.get()][0]
     # get the servers
@@ -233,33 +238,42 @@ def on_country_change():
     server_groupChosen.state(['!disabled'])
     if len(server_groupChosen['values']) == 1:
         server_groupChosen.current(0)
-        configsId = json.loads(server_groups)[0]["configsId"],
-        groupsId = json.loads(server_groups)[0]["groupsId"],
-        config_domain_text.set(
-            # configsId-groupsId-countrycode.cg-dialup.net
-            configsId[0]+"-"+groupsId[0]+"-"+country_code.lower()+".cg-dialup.net")
-        
+        set_config_domain(0)
+    else:
+        # clear the group combobox
+        server_groupChosen.set("")
+    
+
+def set_config_domain(i):
+    configsId = json.loads(server_groups)[i]["configsId"],
+    groupsId = json.loads(server_groups)[i]["groupsId"],
+    config_domain_text.set(
+        # configsId-groupsId-countrycode.cg-dialup.net
+        configsId[0]+"-"+groupsId[0]+"-"+country_code.lower()+".cg-dialup.net")      
+    
 countryChosen.bind("<<ComboboxSelected>>", lambda e: on_country_change())
 
 # create a combobox to choose the server group
-ttk.Label(labelsFrame2, text="Server group:").grid(column=0, row=1, sticky='W')
+ttk.Label(labelsFrame2, text="Server group:").grid(column=0, row=2, sticky='W')
 server_group_text = tk.StringVar()
 server_groupChosen = ttk.Combobox(
     labelsFrame2, width=52, textvariable=server_group_text, state='readonly')
-server_groupChosen.grid(column=1, row=1)
+server_groupChosen.grid(column=1, row=2)
+server_groupChosen.state(['disabled'])
+server_groupChosen.bind("<<ComboboxSelected>>", lambda e: set_config_domain(server_groupChosen.current()))
 
 # config domain
 # THIS IS A SUPPOSITION, NOTHING IS WRITTEN ABOUT IT, but i seems to work
 # configsId-groupsId-countrycode.cg-dialup.net
 # ex : 97-1-ad.cg-dialup.net
 
-ttk.Label(labelsFrame2, text="Config domain:").grid(column=0, row=2, sticky='W')
+ttk.Label(labelsFrame2, text="Config domain:").grid(column=0, row=3, sticky='W')
 config_domain_text = tk.StringVar()
 config_domain_text.set("**-*-*.cg-dialup.net")
 # this text is selectable, but not editable
 config_domain = ttk.Entry(labelsFrame2, width=20, textvariable=config_domain_text,
                             state='readonly')
-config_domain.grid(column=1, row=2)
+config_domain.grid(column=1, row=3)
 
 
 
